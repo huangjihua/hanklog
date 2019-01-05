f7ada32d9094a233bc250e2d54c8933b428e1ce0
hank <steven-online@hotmail.com>
2019-01-05 09:00:43 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<huangjihua>/<huangjihua>.github.io.git master
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<huangjihua>/<REPO>.git master:gh-pages
+#git push -f git@github.com:<huangjihua>/<REPO>.git master:gh-pages
 
 
 #git subtree push --prefix=dist origin gh-pages
